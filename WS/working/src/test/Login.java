bc87f23daa546c52842308c1ffa1b8950f4b611b
joshpeterson1 <joshpeterson6.0@gmail.com>
2018-08-14 08:39:46 -0600

    see details
    moved working dir to "working" in WS

--- WS/working/src/test/Login.java
+++ WS/working/src/test/Login.java
@@ -5,33 +5,32 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
-import com.gargoylesoftware.htmlunit.javascript.host.Console;
-public class SolutionReachLogin {
+public class Login {
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.gecko.driver", "C:\\\\Users\\xlive\\Documents\\1KMS\\sel\\Selenium\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", "C:\\\\Users\\xlive\\Documents\\1KMS\\Selenium\\geckodriver.exe");
 		// Create a new instance of the Firefox driver
 		WebDriver driver = new FirefoxDriver();
 		//  Wait For Page To Load
 		// Put a Implicit wait, this means that any search for elements on the page could take the time the implicit wait is set for before throwing exception driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		// Navigate to URL
-		driver.get("https://login.solutionreach.com");
+		driver.get("https://facebook.com");
 		// Maximize the window.
 		driver.manage().window().maximize();
 		// Enter UserName
-		driver.findElement(By.id("okta-signin-username")).sendKeys("peteboys3@comcast.net");
+		driver.findElement(By.id("email")).sendKeys("peteboys3@comcast.net");
 		// Enter Password
-		driver.findElement(By.id("okta-signin-password")).sendKeys("wrong123");
+		driver.findElement(By.id("pass")).sendKeys("wrong123");
 		// Wait For Page To Load
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		// Click on 'Sign In' button
-		driver.findElement(By.id("okta-signin-submit")).click();
+		driver.findElement(By.id("u_0_2")).click();
 		//Find Incorrect Login DIV.
-		//driver.findElement(By.xpath("//span/p[contains(text(),'Sign in failed!')]")).click(); //The password you’ve entered is incorrect.
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-		WebElement errorPopup = driver.findElement(By.xpath("//p[contains(text(),'Sign in failed!')]"));
-		boolean isPresent = errorPopup.isDisplayed();
-		System.out.println("Login Failed: " + isPresent);
+		driver.findElement(By.xpath("//div[contains(text(),'The password you’ve entered is incorrect. ')]")).click(); //The password you’ve entered is incorrect.
+		//WebElement errorPopup = driver.findElement(By.xpath("//div[contains(text(),'The password you’ve entered is incorrect. ')]"));
+		boolean isPresent = false; 
+		if (isPresent = false) {
+			isPresent = driver.findElements(By.xpath("//div[contains(text(),'The password you’ve entered is incorrect. ')]")).size() > 0;
+		 }
 
 		//Boolean isPresent = errorPopup.getSize() > 0;
 		
@@ -40,6 +39,6 @@ public class SolutionReachLogin {
 		//Click on 'Logout' Button
 		//driver.findElement(By.xpath("//*[@id='gb_71']")).click();
 		//Close the browser.
-		
+		driver.close();
 	}
 }
